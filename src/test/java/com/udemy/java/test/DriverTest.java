56e624d272549c8822d3dd786afb60653f1c84ff
ufry <xyzprueba1@gmail.com>
2021-05-24 20:58:27 -0400

    Architect GitDemo

--- src/test/java/com/udemy/java/test/DriverTest.java
+++ src/test/java/com/udemy/java/test/DriverTest.java
@@ -1,6 +1,21 @@
 package com.udemy.java.test;
 
+import org.testng.annotations.Test;
+
 public class DriverTest {
 
-    
+    @Test
+    public void NativeAPpAndroid() {
+        System.out.println("Hello NativeAPpAndroid");
+    }
+
+    @Test
+    public void NativeAPpAndroidS() {
+        System.out.println("Hello NativeAPpAndroidS");
+    }
+
+    @Test
+    public void IOApps() {
+        System.out.println("Hello IOApps");
+    }
 }
