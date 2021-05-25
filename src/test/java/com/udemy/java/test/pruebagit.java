b06bd177a6f6031551ea11af22b6c34009e548cd
ufry <xyzprueba1@gmail.com>
2021-05-24 21:26:27 -0400

    Architect GitX

---
+++ src/test/java/com/udemy/java/test/pruebagit.java
@@ -0,0 +1,11 @@
+package com.udemy.java.test;
+
+import org.testng.annotations.Test;
+
+public class pruebagit {
+
+    @Test
+    public void IOApps() {
+        System.out.println("Hello IOApps");
+    }
+}
